6f13fcfc6ba9ea236fc50277cc05b9d6b744f24b
Shavie5 <xavier.arechavalas@educem.net>
2024-10-30 16:03:41 +0100

    Assignació de tecles
    Afegim tecles pel moviment del jugador.

--- Player.gd
+++ Player.gd
@@ -1,3 +1,6 @@
 extends Area2D
 export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
 var screen_size # Mida de la finestra de joc.
+
+func _ready():
+	screen_size = get_viewport_rect().size
